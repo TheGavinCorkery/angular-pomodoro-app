17ae2a507a6907475d653ce44e80416a9f0c0c2a
Gavin Corkery <GavinCorkery@hotmail.com>
2022-01-10 20:21:52 -0500

    added imports for webclient and service to handle CRUD operations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,18 +9,22 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { TimerComponent } from './components/timer/timer.component';
+import { NewTimerComponent } from './new-timer/new-timer.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    TimerComponent
+    TimerComponent,
+    NewTimerComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    CountdownModule
+    CountdownModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent, NavbarComponent]
